1b9904eb6e5a137536e498456c7c90ec3fc53d05
Mangassouba <hamallahmanga@gmail.com>
2024-10-04 13:07:43 +0000

    modification le fichier index

--- index.js
+++ index.js
@@ -4,16 +4,17 @@ express
 const app = express();
 const port = 3000;
 
-const API_KEY=123456
+const API_KEY="123456"
 
 const checkApiKey = (req, res, next) => {
   const apiKey = req.headers['x-api-key'];
 
   if (!apiKey) {
-      return res.status(403).json({ error: 'API key is missing' });
+     res.status(403).json({ error: 'API key is missing' });
+     return ;
   }
 
-  if (apiKey !== process.env.API_KEY) {
+  if (apiKey !== API_KEY) {
       return res.status(403).json({ error: 'Invalid API key' });
   }
 
